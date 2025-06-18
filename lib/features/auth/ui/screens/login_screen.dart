4be59e8aaec4e60ed06824adbe1c765a5014dfb3
hade-allaila <hade.allaila@gmail.com>
2025-06-18 16:16:37 +0300

    -added login screen widget

---
+++ lib/features/auth/ui/screens/login_screen.dart
@@ -0,0 +1,22 @@
+import 'package:chatting_app/core/theming/app_text_styles.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            height: 500.h,
+            decoration: BoxDecoration(),
+          ),
+        ],
+      ),
+    );
+  }
+}
